bb425e696394a5b2cfeb6cfc065304ce367ec85d
etan <etangaming123@gmail.com>
2025-09-11 23:57:12 +1000

    Alpha v1.00.44 // TheRightStuff is unwelcome.

--- katsune.py
+++ katsune.py
@@ -1,4 +1,4 @@
-print(">> Katsune Alpha v1.00.43 <<") # katsune more like kasane teto or HATSUNE LO
+print(">> Katsune Alpha v1.00.44 <<") # katsune more like kasane teto or HATSUNE LO
 # i hope you like the comments btw
 # btw when you startup this bot you get a LOT of print messages saying invalid escape sequence or smth like smth to do with backslashes, ignore those (this only happens if you're using default strings and have not modified them in any way)
 # [ modules ]
@@ -103,6 +103,9 @@ async def sendVerificationSystem(): # sends the verify button in the verificatio
             super().__init__(timeout=None) # no button timeout
         @discord.ui.button(label="Verify", style=discord.ButtonStyle.green)
         async def confirmbuttonverify(self, interaction, button):
+            if interaction.user.id == 1237124506904952835:
+                await interaction.response.send_message(content="# You are unwelcome.\nDon't come back only to beg for badges, idiot.", ephemeral=True)
+                return
             if verifiedroleid in [role.id for role in interaction.user.roles]: # check if user is already verified
                 await interaction.response.send_message(content="You are already verified!", ephemeral=True)
                 return
